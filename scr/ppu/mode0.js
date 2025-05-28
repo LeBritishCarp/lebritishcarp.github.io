1199795f53ffd130e3e92963904b7cd0d09f95ea
LeBritishCarp <168266434+LeBritishCarp@users.noreply.github.com>
2025-05-28 21:08:07 +0400

    Create mode0.js

---
+++ scr/ppu/mode0.js
@@ -0,0 +1,5 @@
+// src/ppu/mode0.js
+export function renderMode0(memory, io, imageData) {
+  // TODO: implement BG0–3 tile layers using tile maps, tile data, palettes
+  // See DISPCNT bits for BG enables & priorities, BGnCNT for base addresses, etc.
+}

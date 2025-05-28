67283c3d1414d129e03516003fed3a3f9bdfb504
LeBritishCarp <168266434+LeBritishCarp@users.noreply.github.com>
2025-05-28 21:06:53 +0400

    Update index.js

--- scr/ppu/index.js
+++ scr/ppu/index.js
@@ -1,22 +1,34 @@
 // src/ppu/index.js
+import { renderMode0 } from './mode0.js';
+import { renderMode3 } from './mode3.js';
+
 export class PPU {
-  constructor(memory, ctx) {
-    this.memory = memory;
-    this.ctx = ctx;
-    // prepare an ImageData buffer for 240×160 pixels
+  constructor(mem, io, ctx) {
+    this.memory    = mem;
+    this.io        = io;
+    this.ctx       = ctx;
     this.imageData = ctx.createImageData(240, 160);
   }
 
+  reset() {
+    // nothing to clear here yet
+  }
+
   render() {
-    // For now: clear to solid magenta so you can see updates
-    const data = this.imageData.data;
-    for (let i = 0; i < data.length; i += 4) {
-      data[i]   = 255; // R
-      data[i+1] =   0; // G
-      data[i+2] = 255; // B
-      data[i+3] = 255; // A
+    const dispcnt = this.io.ppu.readDISPCNT();
+    const mode    = dispcnt & 0x7;
+    switch (mode) {
+      case 0:
+        renderMode0(this.memory, this.io, this.imageData);
+        break;
+      case 3:
+        renderMode3(this.memory, this.imageData);
+        break;
+      default:
+        // clear to black
+        const d = this.imageData.data;
+        for (let i = 0; i < d.length; i += 4) d[i + 3] = 0;
     }
-    // draw the buffer onto the canvas (scaled up automatically by CSS/HTML)
     this.ctx.putImageData(this.imageData, 0, 0);
   }
 }

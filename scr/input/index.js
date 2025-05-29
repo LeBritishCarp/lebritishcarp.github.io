18f4b32a5b372162ef51088fee44d60b2d90d6ad
LeBritishCarp <168266434+LeBritishCarp@users.noreply.github.com>
2025-05-29 08:49:02 +0400

    Update index.js

--- scr/input/index.js
+++ scr/input/index.js
@@ -1,15 +1,31 @@
 // src/input/index.js
 export class Input {
-  constructor(memory, io) {
-    this.memory = memory;
-    this.io     = io;
-    this.state  = 0x03FF; // all released
-  }
-  reset() {
+  constructor(mem, io) {
+    this.io = io; this.mem = mem;
     this.state = 0x03FF;
+    window.addEventListener('keydown', e => this.setKey(e.code, false));
+    window.addEventListener('keyup',   e => this.setKey(e.code, true));
   }
-  step() {
-    // TODO: poll keyboard or on-screen buttons,
-    //       update this.state, write to IO register 0x04000130
+  reset() { this.state = 0x03FF; this.io.keypad.irq=false; }
+  setKey(code, released) {
+    const maskMap = {
+      ArrowUp:    0x0400,
+      ArrowDown:  0x0800,
+      ArrowLeft:  0x0200,
+      ArrowRight: 0x0100,
+      KeyZ:       0x0010, // A
+      KeyX:       0x0020, // B
+      Enter:      0x0004, // Start
+      ShiftRight: 0x0008, // Select
+    };
+    if (!(code in maskMap)) return;
+    const mask = maskMap[code];
+    const old  = this.state;
+    this.state = released ? this.state|mask : this.state&~mask;
+    this.io.keypad.value = this.state;
+    if ((old ^ this.state) & this.io.keypad.irqEnable) {
+      this.io.keypad.irq = true;
+    }
   }
+  step() { /* nothing else needed */ }
 }

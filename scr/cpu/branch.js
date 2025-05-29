faae6b5cbe70b63cc3a3bb354d5fa2b007f4eee0
LeBritishCarp <168266434+LeBritishCarp@users.noreply.github.com>
2025-05-29 09:08:56 +0400

    Update branch.js

--- scr/cpu/branch.js
+++ scr/cpu/branch.js
@@ -1,9 +1,9 @@
 // src/cpu/branch.js
 export function handleBranch(insn, cpu) {
-  const L   = !!(insn & (1<<24));
-  let offset = (insn & 0x00FFFFFF) << 2;
+  const L     = !!(insn & (1<<24));
+  let offset  = (insn & 0x00FFFFFF) << 2;
   if (offset & 0x02000000) offset |= 0xFC000000; // sign-extend
-  const pc = cpu.regs[15];
+  const pc    = cpu.regs[15];
   if (L) cpu.regs[14] = pc;
   cpu.regs[15] = (pc + offset) >>> 0;
   return 3;

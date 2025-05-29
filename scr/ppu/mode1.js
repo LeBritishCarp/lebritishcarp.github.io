a0b31b5b5689bf4e532f15a904263a55f1c6b846
LeBritishCarp <168266434+LeBritishCarp@users.noreply.github.com>
2025-05-29 14:41:56 +0400

    Update mode1.js

--- scr/ppu/mode1.js
+++ scr/ppu/mode1.js
@@ -2,7 +2,5 @@
 import { renderMode0 } from './mode0.js';
 
 export function renderMode1(mem, io, imageData) {
-  // Mode 1 supports one affine BG (1) and one regular BG (0),
-  // plus optional BG2 tile layer. For now we fallback to Mode0
   renderMode0(mem, io, imageData);
 }

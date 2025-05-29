755f1be3b960588fa7459a02091fa1392e1b41b7
LeBritishCarp <168266434+LeBritishCarp@users.noreply.github.com>
2025-05-29 09:02:06 +0400

    Update multiply.js

--- scr/cpu/multiply.js
+++ scr/cpu/multiply.js
@@ -1,14 +1,15 @@
 // src/cpu/multiply.js
 export function handleMul(insn, cpu) {
-  const A  = !!(insn & (1<<21));    // accumulate
+  const A  = !!(insn & (1<<21));
   const S  = !!(insn & (1<<20));
-  const Rd = (insn>>>16)&0xF;
-  const Rn = (insn>>>12)&0xF;
+  const Rd = (insn >>> 16) & 0xF;
+  const Rn = (insn >>> 12) & 0xF;
+  const Rs = (insn >>> 8)  & 0xF;
   const Rm = insn & 0xF;
-  const Rs = (insn>>>8)&0xF;
-  let res = cpu.regs[Rm] * cpu.regs[Rs];
-  if (A) res = (res + cpu.regs[Rn])>>>0;
-  cpu.regs[Rd] = res>>>0;
+
+  let res = (cpu.regs[Rm] * cpu.regs[Rs]) >>> 0;
+  if (A) res = (res + cpu.regs[Rn]) >>> 0;
+  cpu.regs[Rd] = res;
   if (S) {
     cpu.cpsr = (cpu.cpsr & ~((1<<31)|(1<<30)))
              | ((res>>>31)<<31)

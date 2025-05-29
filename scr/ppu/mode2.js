acb3e6fcda3894f8680cafb14bd8930032de9894
LeBritishCarp <168266434+LeBritishCarp@users.noreply.github.com>
2025-05-29 14:42:36 +0400

    Update mode2.js

--- scr/ppu/mode2.js
+++ scr/ppu/mode2.js
@@ -2,7 +2,5 @@
 import { renderMode0 } from './mode0.js';
 
 export function renderMode2(mem, io, imageData) {
-  // Mode 2 has two affine BGs (0 & 1). As a placeholder,
-  // we render them like regular tile layers.
   renderMode0(mem, io, imageData);
 }

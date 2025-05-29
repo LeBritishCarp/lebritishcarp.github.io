0b7abce2e2ca5ab578d399f763654e8739479495
LeBritishCarp <168266434+LeBritishCarp@users.noreply.github.com>
2025-05-29 09:09:11 +0400

    Update swi.js

--- scr/cpu/swi.js
+++ scr/cpu/swi.js
@@ -1,8 +1,7 @@
 // src/cpu/swi.js
 export function handleSWI(insn, cpu) {
-  // save return
   cpu.regs[14] = cpu.regs[15];
-  cpu.regs[15] = 0x08;    // BIOS SWI vector
+  cpu.regs[15] = 0x08;   // BIOS SWI vector
   cpu.ime = false;
   return 5;
 }

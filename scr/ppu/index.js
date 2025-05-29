662850c9f3e1aaf5527a7fdd442b95d9f210b189
LeBritishCarp <168266434+LeBritishCarp@users.noreply.github.com>
2025-05-29 14:27:49 +0400

    Update index.js

--- scr/ppu/index.js
+++ scr/ppu/index.js
@@ -5,6 +5,7 @@ import { renderMode2 } from './mode2.js';
 import { renderMode3 } from './mode3.js';
 import { renderMode4 } from './mode4.js';
 import { renderMode5 } from './mode5.js';
+import { renderSprites } from './sprites.js';
 
 export class PPU {
   constructor(memory, io, ctx) {
@@ -15,12 +16,14 @@ export class PPU {
   }
 
   reset() {
-    // nothing to clear
+    // nothing to clear here
   }
 
   render() {
     const dispcnt = this.io.ppu.readDISPCNT();
     const mode    = dispcnt & 0x7;
+
+    // Backgrounds & bitmaps
     switch (mode) {
       case 0: renderMode0(this.mem, this.io, this.imageData); break;
       case 1: renderMode1(this.mem, this.io, this.imageData); break;
@@ -28,9 +31,13 @@ export class PPU {
       case 3: renderMode3(this.mem, this.imageData);          break;
       case 4: renderMode4(this.mem, this.io, this.imageData); break;
       case 5: renderMode5(this.mem, this.io, this.imageData); break;
-      default:
-        this._clearBlack();
+      default: this._clearBlack();
     }
+
+    // Sprites (must draw after BGs)
+    renderSprites(this.mem, this.io, this.imageData);
+
+    // Present everything
     this.ctx.putImageData(this.imageData, 0, 0);
   }
 

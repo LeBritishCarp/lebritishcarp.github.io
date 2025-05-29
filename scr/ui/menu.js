8786d86436e4c69dcb0e553bbfb9fee60a365eca
LeBritishCarp <168266434+LeBritishCarp@users.noreply.github.com>
2025-05-29 08:57:57 +0400

    Update menu.js

--- scr/ui/menu.js
+++ scr/ui/menu.js
@@ -3,9 +3,7 @@ export function setupMenu(onSelect) {
   const menu = document.getElementById('menu');
   menu.querySelectorAll('button').forEach(btn => {
     btn.addEventListener('click', () => {
-      const emu = btn.dataset.emu;
-      menu.style.display = 'none';
-      onSelect(emu);
+      onSelect(btn.dataset.emu);
     });
   });
 }
